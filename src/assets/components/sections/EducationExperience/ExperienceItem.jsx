1924d26dbca8dbbcd7b04ff201e026851b415567
ichramsyah <iamabdulrahman15@gmail.com>
2025-06-10 06:05:40 +0700

    Update Experience

--- src/assets/components/sections/EducationExperience/ExperienceItem.jsx
+++ src/assets/components/sections/EducationExperience/ExperienceItem.jsx
@@ -11,7 +11,7 @@ const ExperienceItem = ({ logo, position, company, period, responsibilities }) =
     <div className="w-4 h-4 bg-purple-600 rounded-full absolute -left-2 " />
     <div className="flex-grow pl-10 md:pl-12">
       <div className="bg-[#1D042F] p-6 rounded-xl shadow-lg border border-purple-700 flex md:flex-nowrap flex-wrap items-start space-x-6 space-y-5">
-        {logo && <img src={logo} alt={company} className="w-20 h-20 object-contain rounded-lg bg-white p-2 flex-shrink-0" />}
+        {logo && <img src={logo} alt={company} className="w-20 h-20 object-contain rounded-lg bg-white  flex-shrink-0" />}
         <div>
           <h4 className="text-xl md:text-2xl font-semibold text-white mb-2">{position}</h4>
           <p className="text-lg text-gray-300 mb-1">{company}</p>
